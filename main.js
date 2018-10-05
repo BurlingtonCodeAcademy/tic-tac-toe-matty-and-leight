13e388b2e57deb33df0132ea424cc71a1ebb9b8a
Leight Johnson <leightjohnson93@gmail.com>
2018-10-05 11:44:48 -0400

    computer plays

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 class TicTacToe {
-
   constructor() {
     this.turn;
     this.currentPlayer;
@@ -12,55 +11,87 @@ class TicTacToe {
       let cell = document.getElementById(`cell-${i}`);
       boardArray.push(cell.textContent);
     }
-    if (!boardArray.slice(0,3).includes('') ||
-        !boardArray.slice(3,6).includes('') ||
-        !boardArray.slice(6,9).includes('')) {
-          document.querySelector('h3').textContent = `${this.currentPlayer} Wins!`;
+    if (
+      this.boardState(boardArray, 0, 1, 2) ||
+      this.boardState(boardArray, 3, 4, 5) ||
+      this.boardState(boardArray, 6, 7, 8) ||
+      this.boardState(boardArray, 0, 4, 8) ||
+      this.boardState(boardArray, 2, 4, 6) ||
+      this.boardState(boardArray, 0, 3, 6) ||
+      this.boardState(boardArray, 1, 4, 7) ||
+      this.boardState(boardArray, 2, 5, 8)
+    ) {
+      document.querySelector("h3").textContent = `${this.currentPlayer} Wins!`;
+      return true;
+    }
+    return false;
+  }
+
+  boardState(boardArray, firstCell, secondCell, thirdCell) {
+    if (
+      boardArray[firstCell] === this.turn &&
+      boardArray[secondCell] === this.turn &&
+      boardArray[thirdCell] === this.turn
+    ) {
       return true;
     }
   }
 
   start() {
     this.players = {
-      X: document.getElementById('player-1').value,
-      O: document.getElementById('player-2').value
-    }
+      X: document.getElementById("player-1").value,
+      O: document.getElementById("player-2").value
+    };
     if (Math.round(Math.random()) === 0) {
       this.currentPlayer = this.players.X;
-      this.turn = 'X';
+      this.turn = "X";
     } else {
       this.currentPlayer = this.players.O;
-      this.turn = 'O';
+      this.turn = "O";
     }
 
-    document.querySelector('h3').textContent = `Ready ${this.currentPlayer}`;
+    document.querySelector("h3").textContent = `Ready ${this.currentPlayer}`;
   }
 
+  computer() {
+    document.getElementById("player-2").value = "Computer";
+      if (this.turn === "O") {
+        let randomCell = document.getElementById(
+          `cell-${Math.floor(Math.random() * 9)}`
+        )
+        randomCell.textContent = this.turn;
+          console.log('function call')
+          console.log(randomCell)
+        console.log('computer turn')
+        this.toggle();
+      }
+    }
+
   toggle() {
-    if (this.turn === 'X') {
-      this.turn = 'O';
+    if (this.turn === "X") {
+      this.turn = "O";
       this.currentPlayer = this.players.O;
-      document.querySelector('h3').textContent = `Ready ${this.currentPlayer}`;
-    } else if (this.turn === 'O') {
-      this.turn = 'X'
+      document.querySelector("h3").textContent = `Ready ${this.currentPlayer}`;
+    } else if (this.turn === "O") {
+      this.turn = "X";
       this.currentPlayer = this.players.X;
-      document.querySelector('h3').textContent = `Ready ${this.currentPlayer}`;
+      document.querySelector("h3").textContent = `Ready ${this.currentPlayer}`;
     }
   }
 
   setCell(cell) {
-    if (cell.textContent !== '') {
-      console.log("text content");
-      console.log(cell.textContent);
-      alert("Impossible! That cell is already full.")
-      return
+    if (cell.textContent !== "") {
+      alert("Impossible! That cell is already full.");
+      return;
     }
     cell.textContent = this.turn;
     if (this.winCheck()) {
-      this.turn = '';
-      return
+      this.turn = "";
+      return;
     }
-    this.toggle();
+    if (document.getElementById("computer").checked) {
+      this.computer();
+    }
   }
 }
 
--- play.js
+++ play.js
@@ -1,16 +1,17 @@
 const game = new TicTacToe();
 
 function play() {
-  console.log("CLICKED!")
   for (let i = 0; i <= 8; i++) {
     let cell = document.getElementById(`cell-${i}`);
     cell.onclick = function() {
-      console.log({game})
       game.setCell(cell);
-     }
-    cell.textContent = '';
+    };
+    cell.textContent = "";
+    game.start();
+    if (document.getElementById("computer").checked) {
+      game.computer();
+    }
   }
-  game.start();
 }
 
-document.querySelector('button').onclick = play;
+document.querySelector("button").onclick = play;
